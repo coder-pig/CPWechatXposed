daa7d47da75209adb31f83f1a91069c2b223130b
CoderPig <779878443@qq.com>
2018-04-03 19:02:01 +0800

    create simple demo

---
+++ app/src/main/java/com/coderpig/cpwechatxposed/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coderpig.cpwechatxposed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+        tv.setText("渣渣辉");
+    }
+}
---
+++ app/src/main/java/com/coderpig/cpwechatxposed/XposedInit.java
@@ -0,0 +1,43 @@
+package com.coderpig.cpwechatxposed;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * 描述：
+ *
+ * @author CoderPig on 2018/04/03 18:51.
+ */
+
+public class XposedInit implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.coderpig.cpwechatxposed")) {
+            XposedHelpers.findAndHookMethod("com.coderpig.cpwechatxposed.MainActivity",
+                    lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                        }
+
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            Class c = lpparam.classLoader.loadClass("com.coderpig.cpwechatxposed.MainActivity");
+                            Field field = c.getDeclaredField("tv");
+                            field.setAccessible(true);
+                            Log.e("Test","呵呵");
+                            TextView tv = (TextView) field.get(param.thisObject);
+                            tv.setText("大傻逼");
+                        }
+                    });
+        }
+    }
+}
